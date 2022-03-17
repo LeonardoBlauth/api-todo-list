7ac87d3d671cd09305e3b95e6db345b51299dd81
LeoBlauth <leonardo.blauth@dealerup.com.br>
2022-03-16 23:15:37 -0300

    generate fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,14 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Task::factory(100)->create();
     }
 }
