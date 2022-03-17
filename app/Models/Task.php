410e76602cfc2b5260af607e10357fa513277c69
LeoBlauth <leonardo.blauth@dealerup.com.br>
2022-03-17 02:42:55 -0300

    cast values

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,4 +18,36 @@ class Task extends Model
         'end_date',
         'state',
     ];
+
+    protected $casts = [
+        'state' => 'boolean',
+    ];
+
+    protected function status(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value) {
+                switch ($value) {
+                    case 0:
+                        return ["style" => 'new', "label" => 'New'];
+                    case 1:
+                        return ["style" => "in-progress", "label" => 'In Progress'];
+                    case 2:
+                        return ["style" => "on-pause", "label" => 'On Pause'];
+                    case 3:
+                        return ["style" => "completed", "label" => 'Completed'];
+                    case 4:
+                        return ["style" => "completed-advance", "label" => 'Completed Advance'];
+                    case 5:
+                        return ["style" => "completed-late", "label" => 'Completed Late'];
+                    case 6:
+                        return ["style" => "late", "label" => 'Late'];
+                    case 7:
+                        return ["style" => "abandoned", "label" => 'Abandoned'];
+                    default:
+                        return ["style" => "new", "label" => "New"];
+                }
+            }
+        );
+    }
 }
